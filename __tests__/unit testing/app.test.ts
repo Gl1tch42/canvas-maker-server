382657c6c8283c91d4902f1e7dbf74a990750648
Vinicius Souza <me@viniciusals.com>
2020-07-07 13:29:26 -0300

    Change tests names to make it more clear what test does

--- __tests__/unit testing/app.test.ts
+++ __tests__/unit testing/app.test.ts
@@ -5,19 +5,19 @@ import app from '../../src/app';
 
 describe('Check api responses:', () => {
 
-	it('Responds 204 to api/', done => {
+	it('Responds 204 when request GET to api/', done => {
 		request(app)
 			.get('/api')
 			.expect(204, done);
 	});
 
-	it('Responds 404 to everything else', done => {
+	it('Responds 404 when request GET to wrong route', done => {
 		request(app)
 			.get('/wrong-random-path')
 			.expect(404, done);
 	});
 
-	it('Responds 422 to api/signup with incorect request body', done => {
+	it('Responds 422 when request POST to api/signup with incorect request body', done => {
 		request(app)
 			.get('/api/signup')
 			.expect(422, done);
