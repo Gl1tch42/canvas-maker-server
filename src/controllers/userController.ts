031596bf72acc96a2360a116a1488aa293fca395
Vinicius Souza <me@viniciusals.com>
2020-07-07 13:47:18 -0300

    Refactor userController

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,13 +1,11 @@
 /* eslint-disable no-mixed-spaces-and-tabs */
 import { Request, Response } from 'express';
 
-class UserController {
+export = {
 
-    signUp = async (req: Request, res: Response): Promise<Response> => {
+	signUp: async (req: Request, res: Response): Promise<Response> => {
 
     	await console.log('UsersController.signUp() called.');
     	return res.status(200).json({ body: req.body });
-    };
+	},
 }
-
-export default new UserController;
