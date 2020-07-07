e572f5a4ca95ea415a3be7789c00c6fcff2f60c9
Vinicius Souza <me@viniciusals.com>
2020-07-07 10:34:41 -0300

    Create route for signup

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,15 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 
 const routes = Router();
 
 routes.get('/', (_, res:Response) => {
 	res.status(204).send();
+});
+
+routes.post('/signup', async (req:Request, res:Response) => {
+	await console.log('UsersController.signUp() called.');
+
+	return res.status(200).json({ body: req.body });
 });
 
 export default routes;
