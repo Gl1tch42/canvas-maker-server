0f80cf2cfdcaaa1227ac46e531e64b2f90584c82
Vinicius Souza <me@viniciusals.com>
2020-07-07 10:43:38 -0300

    imports signUp function from userController

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router, Response } from 'express';
+import { signUp } from './controllers/userController';
 
 const routes = Router();
 
@@ -6,10 +7,6 @@ routes.get('/', (_, res:Response) => {
 	res.status(204).send();
 });
 
-routes.post('/signup', async (req:Request, res:Response) => {
-	await console.log('UsersController.signUp() called.');
-
-	return res.status(200).json({ body: req.body });
-});
+routes.post('/signup', signUp);
 
 export default routes;
