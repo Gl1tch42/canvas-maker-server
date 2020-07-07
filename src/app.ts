70b736027c2bcabaa4f582f801fa84211c2b1adf
Vinicius Souza <me@viniciusals.com>
2020-07-06 22:07:36 -0300

    Remove xss-clean

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,10 @@
 import express from 'express';
-import xss from 'xss-clean';
 
 import routes from './routes';
 
 const app = express();
 
 app.use(express.json({ limit: '10kb' }));
-app.use(xss());
 
 app.use('/api', routes);
 
