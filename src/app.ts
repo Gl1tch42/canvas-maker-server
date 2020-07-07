d4edddc1f44d70d4c27ce242d7127cfd13ae39e0
Vinicius Souza <me@viniciusals.com>
2020-07-06 22:11:52 -0300

    Uses helmet as a middleware for express api

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import express from 'express';
+import helmet from 'helmet';
 
 import routes from './routes';
 
 const app = express();
 
 app.use(express.json({ limit: '10kb' }));
+app.use(helmet());
 
 app.use('/api', routes);
-
 
 export default app;
