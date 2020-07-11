8a9faaa13c36022161b9389d0b87b814c2641c96
Vinicius Souza <me@viniciusals.com>
2020-07-11 13:17:10 -0300

    Creates tests for signup route

--- __tests__/unit testing/app.test.ts
+++ __tests__/unit testing/app.test.ts
@@ -3,23 +3,95 @@
 import request from 'supertest';
 import app from '../../src/app';
 
-describe('Check api responses:', () => {
+describe('Checks if API is up:', () => {
 
-	it('Responds 204 when request GET to api/', done => {
+	it('Responds 204 when request GET to api/.', done => {
 		request(app)
 			.get('/api')
 			.expect(204, done);
 	});
 
-	it('Responds 404 when request GET to wrong route', done => {
+	it('Responds 404 when request GET to wrong route.', done => {
 		request(app)
-			.get('/wrong-random-path')
+			.get('/api/wrong-random-path')
 			.expect(404, done);
 	});
+});
 
-	it('Responds 422 when request POST to api/signup with incorect request body', done => {
+describe('Checks POST request on api/signup route:', () => {
+
+	it('Responds 200 to correct request body.', done => {
+
+		const requestBody = {
+			'email': 'me@email.com',
+			'password': 'asdflkjasdlfj',
+		};
+
 		request(app)
 			.post('/api/signup')
+			.send(requestBody)
+			.expect('Content-Type', /json/u)
+			.expect(200, done);
+	});
+
+	it('Responds 422 to empty request body.', done => {
+		request(app)
+			.post('/api/signup')
+			.expect('Content-Type', /json/u)
+			.expect(422, done);
+	});
+
+	it('Responds 422 to incorrect email on request body.', done => {
+
+		const requestBody = {
+			'email': 'meemail.com',
+			'password': 'asdflkjasdlfj',
+		};
+
+		request(app)
+			.post('/api/signup')
+			.send(requestBody)
+			.expect('Content-Type', /json/u)
+			.expect(422, done);
+	});
+
+	it('Responds 422 to incorrect too short password on request body.', done => {
+
+		const requestBody = {
+			'email': 'meemail.com',
+			'password': 'asdj',
+		};
+
+		request(app)
+			.post('/api/signup')
+			.send(requestBody)
+			.expect('Content-Type', /json/u)
+			.expect(422, done);
+	});
+
+	it('Responds 422 to missing email on request body.', done => {
+
+		const requestBody = {
+			'password': 'asdflkjasdlfj',
+		};
+
+		request(app)
+			.post('/api/signup')
+			.send(requestBody)
+			.expect('Content-Type', /json/u)
+			.expect(422, done);
+	});
+
+	it('Responds 422 to missing password on request body.', done => {
+
+		const requestBody = {
+			'email': 'meemail.com',
+		};
+
+		request(app)
+			.post('/api/signup')
+			.send(requestBody)
+			.expect('Content-Type', /json/u)
 			.expect(422, done);
 	});
 });
