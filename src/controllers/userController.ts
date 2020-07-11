1980ab26729d7cd6f709633a741398b2ddbc72da
Vinicius Souza <me@viniciusals.com>
2020-07-11 12:07:08 -0300

    use mysql queries from external file

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,11 +1,15 @@
 /* eslint-disable no-mixed-spaces-and-tabs */
 import { Request, Response } from 'express';
+import userQueries from '../database/userQueries';
+// import connection from '../database/Connection';
 
 export = {
 
 	signUp: async (req: Request, res: Response): Promise<Response> => {
 
-    	await console.log('UsersController.signUp() called.');
-    	return res.status(200).json({ body: req.body });
+		 const users = await userQueries.allUsers();
+		// const [rows, _] = await connection.query('SELECT * FROM userIdentity');
+
+		return res.status(200).json({ saldjflkasjd: users });
 	},
 }
