63cef38db8fe3f76c0ff5921dd8c3a0bdc210b25
Vinicius Souza <me@viniciusals.com>
2020-07-11 18:12:42 -0300

    Refactor userQueries into a class

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,13 +1,13 @@
 /* eslint-disable no-mixed-spaces-and-tabs */
 import { Request, Response } from 'express';
-import userQueries from '../database/userQueries';
+import UserQueries from '../database/userQueries';
 // import connection from '../database/Connection';
 
 export = {
 
 	signUp: async (req: Request, res: Response): Promise<Response> => {
 
-		 const users = await userQueries.allUsers();
+		 const users = await UserQueries.allUsers();
 
 		return res.status(200).json({ saldjflkasjd: users });
 	},
--- src/database/userQueries.ts
+++ src/database/userQueries.ts
@@ -1,10 +1,12 @@
 import connection from './Connection';
 import { RowDataPacket, OkPacket, ResultSetHeader } from 'mysql2';
 
-export = {
-	allUsers: async ():Promise<RowDataPacket[] | RowDataPacket[][] | OkPacket | OkPacket[] | ResultSetHeader> => {
+export default class UserQueries {
+
+	public static async allUsers():Promise<RowDataPacket[] | RowDataPacket[][] | OkPacket | OkPacket[] | ResultSetHeader> {
 		const [rows, _] = await connection.query('SELECT * FROM userIdentity');
 		return rows;
-	},
+	}
+
 }
 
