41900965d473e2d6917f54c7a934a6d2aef90905
Vinicius Souza <me@viniciusals.com>
2020-07-12 15:44:49 -0300

    Change name of DB table on query

--- src/database/UserQueries.ts
+++ src/database/UserQueries.ts
@@ -4,7 +4,7 @@ import { RowDataPacket, OkPacket, ResultSetHeader } from 'mysql2';
 export default class UserQueries {
 
 	public static async allUsers():Promise<RowDataPacket[] | RowDataPacket[][] | OkPacket | OkPacket[] | ResultSetHeader> {
-		const [rows, _] = await connection.query('SELECT * FROM userIdentity');
+		const [rows, _] = await connection.query('SELECT * FROM Accounts');
 		return rows;
 	}
 
