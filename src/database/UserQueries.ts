7b5fee9e2e674153cf895f5e7909c04c4a7ee838
Vinicius Souza <me@viniciusals.com>
2020-07-14 21:24:29 -0300

    Create type interface for User

--- src/database/UserQueries.ts
+++ src/database/UserQueries.ts
@@ -1,6 +1,13 @@
 import connection from './Connection';
 import { RowDataPacket, OkPacket, ResultSetHeader } from 'mysql2';
 
+interface User {
+	name: string,
+	nickname: string,
+	email: string,
+	password: string
+}
+
 export default class UserQueries {
 
 	public static async allUsers():Promise<RowDataPacket[] | RowDataPacket[][] | OkPacket | OkPacket[] | ResultSetHeader> {
