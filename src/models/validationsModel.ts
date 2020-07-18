fdbe973fddffc3cf68f737da75046444766530eb
Vinicius Souza <me@viniciusals.com>
2020-07-18 20:43:53 -0300

    Add new validation scheme to login Local Users

--- src/models/validationsModel.ts
+++ src/models/validationsModel.ts
@@ -27,3 +27,22 @@ export const createLocalUser = [
 		.isLength({ min: 7 })
 		.withMessage('Password field is invalid. Password should be at least 7 characters long.')
 ];
+
+export const loginLocalUser = [
+
+	body('email')
+		.exists()
+		.withMessage('Email field missing.')
+		.if(body('email').exists())
+		.trim()
+		.isEmail()
+		.normalizeEmail()
+		.withMessage('Email field is invalid.'),
+
+	body('password')
+		.exists()
+		.withMessage('Password field missing.')
+		.if(body('password').exists())
+		.isLength({ min: 7 })
+		.withMessage('Password field is invalid. Password should be at least 7 characters long.')
+];
--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,14 @@
 import { Router, Response } from 'express';
 import userController from './controllers/UserController';
 import { validationChecker } from './controllers/validationController';
-import { createLocalUser } from './models/validationsModel';
+import { createLocalUser, loginLocalUser } from './models/validationsModel';
 
 const routes = Router();
 
 routes.get('/', (_, res:Response) => res.status(204).send());
 
 routes.post('/signup', validationChecker(createLocalUser), userController.signUp);
-// routes.post('/signin', userController.signIn);
+routes.post('/signin', validationChecker(loginLocalUser), userController.signIn);
 // routes.get('/secret', userController.secret);
 
 export default routes;
