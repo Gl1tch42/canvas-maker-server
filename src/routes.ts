93998ab287f8cca88751d6f45fc8db7229542cd6
Vinicius Souza <me@viniciusals.com>
2020-07-19 00:09:53 -0300

    Refactor code to improve readability

--- src/routes.ts
+++ src/routes.ts
@@ -7,8 +7,14 @@ const routes = Router();
 
 routes.get('/', (_, res:Response) => res.status(204).send());
 
-routes.post('/signup', validationChecker(ValidationSchemas.createLocalUser), userController.signUp);
-routes.post('/signin', validationChecker(ValidationSchemas.loginLocalUser), userController.signIn);
+routes.post('/signup',
+	validationChecker(ValidationSchemas.createLocalUser),
+	userController.signUp);
+
+routes.post('/signin',
+	validationChecker(ValidationSchemas.loginLocalUser),
+	userController.signIn);
+
 // routes.get('/secret', userController.secret);
 
 export default routes;
