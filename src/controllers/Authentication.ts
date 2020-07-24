1ed4f6f60479a9bbd9734abc2b4a60e3e244ce31
Vinicius Souza <me@viniciusals.com>
2020-07-23 23:30:25 -0300

    Update getToken method to verify token and return user id :heavy_check_mark:

--- src/controllers/Authentication.ts
+++ src/controllers/Authentication.ts
@@ -1,7 +1,9 @@
+import dotenv from 'dotenv';
 import { Request, Response, NextFunction } from 'express';
 import RequestError from '../models/RequestError';
 import jwt from 'jsonwebtoken';
 
+dotenv.config({ path: 'secure/.env' });
 
 interface Token {
 	userId: number,
@@ -24,8 +26,18 @@ export default class Authentication {
 
 		const token = bearesHeader.split(' ')[1];
 
-		req.token = token;
+		jwt.verify(token, process.env.ACCESS_TOKEN_SECRET!, (error, authData) => {
+			if (error) {
+				console.log(error);
+				res.sendStatus(403);
+				return;
+			}
 
-		next();
+			const varifiedToken = <Token>authData;
+
+			req.id = varifiedToken.userId;
+
+			next();
+		});
 	}
 }
